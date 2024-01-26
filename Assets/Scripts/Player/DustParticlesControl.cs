7b19755889eacbc970b4945e1c7b57b68547bea0
GGB <rlqja1018@gmail.com>
2024-01-26 11:24:26 +0900

    feat - 파티클 좌우반전 구현

--- Assets/Scripts/Player/DustParticlesControl.cs
+++ Assets/Scripts/Player/DustParticlesControl.cs
@@ -6,9 +6,22 @@ public class DustParticleControl : MonoBehaviour
 {
     [SerializeField] private bool createDustOnWalk = true;
     [SerializeField] private ParticleSystem dustParticleSystem;
+    Transform particleTransform;
+    Vector3 particlePos;
+
+    private void Start()
+    {
+        particleTransform = dustParticleSystem.transform;
+        particlePos = particleTransform.position;
+    }
 
     public void CreateDustParticles()
     {
+        particlePos.x = Mathf.Abs(particlePos.x);
+        if(GetComponent<SpriteRenderer>().flipX == false)
+            particlePos.x = -particlePos.x;
+        particleTransform.localPosition = particlePos;
+
         if (createDustOnWalk)
         {
             dustParticleSystem.Stop();
