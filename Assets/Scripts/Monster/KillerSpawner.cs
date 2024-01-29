5ed6313ad8743fc698c4cece4fb0193371399614
BakGuno <129590082+BakGuno@users.noreply.github.com>
2024-01-30 00:31:48 +0900

    feature : 전투 부분 낮밤 분리 추가

--- Assets/Scripts/Monster/KillerSpawner.cs
+++ Assets/Scripts/Monster/KillerSpawner.cs
@@ -23,15 +23,22 @@ public class KillerSpawner : MonoBehaviour
 
     void Update()
     {
-        CurrentTime += Time.deltaTime;
-        _pivot = _gameManager.player.transform;
-        if (CurrentTime > KiierSO.delay) //TODO : 나중에 보고 간격 줄이는 방식으로 회피 난이도 올리기
+        if (_gameManager.gameState == GameState.Ground)
         {
-            spawnposX = Random.Range(_pivot.position.x-10,_pivot.position.x+10);
-            spawnposy = _gameManager.player.transform.position.y+13f;
-            RangedAttackData data = KiierSO as RangedAttackData;
-            Vector3 startpos = new Vector3(spawnposX, spawnposy, 0);
-            _projectileManager.ShootBullet(startpos, new Vector2(transform.position.x, -1), data);
+            CurrentTime += Time.deltaTime;
+            _pivot = _gameManager.player.transform;
+            if (CurrentTime > KiierSO.delay) 
+            {
+                spawnposX = Random.Range(_pivot.position.x - 10, _pivot.position.x + 10);
+                spawnposy = _gameManager.player.transform.position.y + 13f;
+                RangedAttackData data = KiierSO as RangedAttackData;
+                Vector3 startpos = new Vector3(spawnposX, spawnposy, 0);
+                _projectileManager.ShootBullet(startpos, new Vector2(transform.position.x, -1), data);
+                CurrentTime = 0;
+            }
+        }
+        else
+        {
             CurrentTime = 0;
         }
     }
