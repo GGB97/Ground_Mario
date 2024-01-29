6e2fd68ffec93f071f39c36daa20085fed9aa7a8
GGB <rlqja1018@gmail.com>
2024-01-29 20:46:04 +0900

    feat - 아이템(코인) 구현
    10/30/50 코인 구현

---
+++ Assets/Scripts/Player/Controller/PlayerResourceController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerResourceController : MonoBehaviour
+{
+    [SerializeField] Resource_Data _data;
+
+    public void ChangeResource(Resource_Data data)
+    {
+        _data.AddResource(data);
+    }
+}
