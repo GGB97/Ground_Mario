ec6840224d69322e11e352a857a726d49ab8406f
GGB <rlqja1018@gmail.com>
2024-01-29 17:37:03 +0900

    refactor - 씬 병합중 손실된 값들 넣어둔것?

--- Assets/Scripts/Player/DustParticlesControl.cs
+++ Assets/Scripts/Player/DustParticlesControl.cs
@@ -7,10 +7,27 @@ public class DustParticleControl : MonoBehaviour
     [SerializeField] private bool createDustOnWalk = true;
     [SerializeField] private ParticleSystem dustParticleSystem;
 
+    SpriteRenderer spriteRenderer;
+    Transform particleTransform;
+    Vector3 particlePos;
+
+    private void Start()
+    {
+        particleTransform = dustParticleSystem.transform;
+        particlePos = particleTransform.localPosition;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     public void CreateDustParticles()
     {
         if (createDustOnWalk)
         {
+            particlePos.x = Mathf.Abs(particlePos.x);
+            if (spriteRenderer.flipX == false)
+                particlePos.x = -particlePos.x;
+
+            particleTransform.localPosition = particlePos;
+
             dustParticleSystem.Stop();
             dustParticleSystem.Play();
         }
