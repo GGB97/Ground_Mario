df3581c06ed439c06d75196a5044b19c14d12e60
BakGuno <129590082+BakGuno@users.noreply.github.com>
2024-01-29 20:23:38 +0900

    refactor : 포탄 속도, 사이즈 랜덤

--- Assets/Scripts/Monster/KillerSpawner.cs
+++ Assets/Scripts/Monster/KillerSpawner.cs
@@ -7,29 +7,31 @@ public class KillerSpawner : MonoBehaviour
     [SerializeField] private AttackSO KiierSO;
 
     private ProjectileManager _projectileManager;
+    private GameManager _gameManager;
+    private Transform _pivot;
+    
+    
     private float spawnposX;
     private float spawnposy;
     private float CurrentTime;
-    
+
     void Start()
     {
         _projectileManager = ProjectileManager.instance;
+        _gameManager = GameManager.Instance;
     }
-    
+
     void Update()
     {
         CurrentTime += Time.deltaTime;
+        _pivot = _gameManager.player.transform;
         if (CurrentTime > KiierSO.delay) //TODO : 나중에 보고 간격 줄이는 방식으로 회피 난이도 올리기
         {
-            int randomspawh = Random.Range(2,7);
-            for (int i = 0; i < randomspawh; i++)
-            {
-                spawnposX = Random.Range(-10, 10);
-                spawnposy = 13f;
-                RangedAttackData data = KiierSO as RangedAttackData;;
-                Vector3 startpos = new Vector3(spawnposX, spawnposy, 0);
-                _projectileManager.ShootBullet(startpos,new Vector2(transform.position.x,-1),data);                
-            }
+            spawnposX = Random.Range(_pivot.position.x-10,_pivot.position.x+10);
+            spawnposy = _gameManager.player.transform.position.y+13f;
+            RangedAttackData data = KiierSO as RangedAttackData;
+            Vector3 startpos = new Vector3(spawnposX, spawnposy, 0);
+            _projectileManager.ShootBullet(startpos, new Vector2(transform.position.x, -1), data);
             CurrentTime = 0;
         }
     }
