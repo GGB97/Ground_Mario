e6e64aa3a036e3dcfaeede020bc7fffb62e35111
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-29 16:06:30 +0900

    Feat: miningTile

--- Assets/Scripts/Entties/Mining.cs
+++ Assets/Scripts/Entties/Mining.cs
@@ -1,44 +1,87 @@
-using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Mining : MonoBehaviour
 {
-    private CharacterController _controller;
     [SerializeField] private Tilemap tilemap;
+    [SerializeField] private GameObject destroyStage;
 
     private LayerMask _levelLayerMask;
-    private Vector3 _targetPos;
+    private Vector3 _mousePos;
     private Vector3 _position;
     private Vector3 _direction;
+    private Vector3Int _targetPos;
+    private Vector3Int? _recentHit;
+    private CustomRuleTile _target;
     
     public float distance = 5f;
     
     private void Awake()
     {
-        _controller = GetComponent<CharacterController>();
         _levelLayerMask = LayerMask.GetMask("Level");
     }
 
-    private void Start()
+    private void Update()
     {
-        _controller.OnAttackEvent += TryMining;
+        TryMining();
     }
 
-    private void TryMining(AttackSO attackSO)
+    private void TryMining()
     {
-        _targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
+        if (!Input.GetMouseButton(0))
+        {
+            DeactivateDestroyStage();
+            _recentHit = null;
+            return;
+        }
+
+        _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         _position = transform.position;
-        _direction = (_targetPos - _position).normalized;
+        _direction = (_mousePos - _position).normalized;
 
         var hit = Physics2D.Raycast(_position, _direction, distance, _levelLayerMask);
-        if (hit.collider)
-        {
-            var target = (CustomRuleTile)tilemap.GetTile(tilemap.WorldToCell(
-                new Vector2(hit.point.x - (hit.normal.x * 0.01f), hit.point.y - (hit.normal.y * 0.01f))));
+        if (!CheckCorrectTile(hit))
+            return;
+        
+        ProcessTileDestruction(_target, _targetPos);
+    }
 
-            
+    private bool CheckCorrectTile(RaycastHit2D hit)
+    {
+        if (!hit.collider)
+        {
+            DeactivateDestroyStage();
+            _recentHit = null;
+            return false;
+        }
+        
+        _targetPos = tilemap.WorldToCell(new Vector2(hit.point.x - (hit.normal.x * 0.01f), hit.point.y - (hit.normal.y * 0.01f)));
+        
+        if (_recentHit == _targetPos)
+        {
+            return false;
+        }
+
+        DeactivateDestroyStage();
+        _recentHit = _targetPos;
+        _target = (CustomRuleTile)tilemap.GetTile(_targetPos);
+        return true;
+    }
+    
+    private void DeactivateDestroyStage()
+    {
+        if (destroyStage.activeSelf)
+            destroyStage.SetActive(false);
+    }
+    
+    private void ProcessTileDestruction(CustomRuleTile target, Vector3Int targetPos)
+    {
+        if (target.isBreakable && !destroyStage.activeSelf)
+        {
+            destroyStage.GetComponent<Animator>().speed = 1f / target.hardness;
+            destroyStage.GetComponent<DestroyTile>().targetPos = targetPos;
+            destroyStage.transform.position = targetPos + new Vector3(0.5f, 0.5f);
+            destroyStage.SetActive(true);
         }
     }
 }
