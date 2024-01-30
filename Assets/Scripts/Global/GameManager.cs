e1eaa96c3ebafcd135dea56711c027575aebb6ae
sskesu <136444697+sskesu@users.noreply.github.com>
2024-01-30 15:00:48 +0900

    Refactor : 업그레이드 수정

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     [SerializeField] public GameObject player;
-
+    [SerializeField] public GameObject playerBase;
     private int _randomSeed;
     public int WaveCnt { get; private set; }
     public event Action OnStartGameEvent;
